18f958b379076104ab948076ea8821b7e024be45
Yu-Chiang Hsu <nocaoper@gmail.com>
2017-03-03 12:07:42 -0500

    play mute sound test

--- src/background.js
+++ src/background.js
@@ -60,6 +60,8 @@ function setupNotification(timer) {
     chrome.storage.local.get({soundType: "tts", soundId: "ring"}, function(object) {
       if (object.soundType == "tts") {
         chrome.tts.speak(timer.desc);
+      } else if (object.soundType == "mute") {
+        audios[1].play();
       } else {
         audios[object.soundId].play();
       }
